7c3a4f47897bfcc2364ca68c21c749e353aff7d1
Giedrius Kudelis <giedrius.kudelis@vinted.com>
2021-03-14 13:52:34 +0200

    Refactor test to check artifact path explicitly

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,49 +1,51 @@
 use std::env;
 use std::fs;
-use std::path::{Path, PathBuf};
+use std::path::Path;
 use tempfile;
 
 use cement::prototypes::file_select;
 use cement::prototypes::artifact;
 
-fn in_tempdir(f: impl FnOnce(&Path) -> ()) {
+fn in_tempdir<T>(f: impl FnOnce(&Path) -> T) -> T {
     let last_cwd = env::current_dir().unwrap();
     let temp_dir = tempfile::tempdir().unwrap();
+
     env::set_current_dir(&temp_dir).unwrap();
-
-    f(temp_dir.as_ref());
-
+    let result = f(temp_dir.as_ref());
     env::set_current_dir(&last_cwd).unwrap();
+
+    result
 }
 
 #[test]
 fn artifact_writes_a_file() {
     in_tempdir(|dirname| {
         // create file for matching
-        let mut dir = PathBuf::from(dirname);
+        let mut dir = dirname.to_path_buf();
         dir.push("content/posts/relative/path");
-        let mut file = PathBuf::from(&dir);
-        file.push("post.md");
+        let mut post = dir.clone();
+        post.push("post.md");
         fs::create_dir_all(&dir).unwrap();
-        fs::File::create(&file).unwrap();
+        fs::File::create(&post).unwrap();
 
         // match file and create artifact
         let posts = file_select::files_matching("content/posts/**/*.md");
         let artifacts = posts.map(|post_path| {
-            let mut artifact_path = PathBuf::from(post_path);
+            let mut artifact_path = post_path;
             artifact_path.set_extension("html");
             artifact::Artifact { location: artifact_path }
         }).collect::<Vec<_>>();
 
-        // write artifact
-        for artifact in &artifacts {
-            artifact.write("some stuff here");
-        }
+        // should only generate a single artifact
+        assert_eq!(artifacts.len(), 1);
 
-        // check artifact exists
-        for artifact in &artifacts {
-            let content = fs::read_to_string(&artifact.location).unwrap();
-            assert_eq!(content, "some stuff here");
-        }
-    });
+        // write artifact
+        for artifact in &artifacts { artifact.write("some stuff here"); }
+
+        // check artifact contents
+        let mut artifact_path = dirname.to_path_buf();
+        artifact_path.push("content/posts/relative/path/post.html");
+        let content = fs::read_to_string(artifact_path).unwrap();
+        assert_eq!(content, "some stuff here");
+    })
 }
